b79022afd67bde638b095e0167826a9a9e2b4d35
Trunker2010 <Trunker2010@users.noreply.github.com>
2021-10-01 20:18:07 +0300

    Перенесено

---
+++ app/src/main/java/com/example/thermalgapcalc_compose/presentation/ui/CardWithTitle.kt
@@ -0,0 +1,37 @@
+package com.example.thermalgapcalc_compose.presentation.ui
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun CardWithTitle(title: String, content: @Composable () -> Unit) {
+    Card(
+        Modifier
+            .padding(all = 8.dp)
+            .shadow(8.dp)
+            .fillMaxWidth(1f)
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(all = 8.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = title, Modifier.padding(bottom = 8.dp),
+                textAlign = TextAlign.Center,
+                style = MaterialTheme.typography.h6
+            )
+            content()
+        }
+    }
+}
