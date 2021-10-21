8f8137135a2102f8de7c41a79879462f59d8f78a
Trunker2010 <Trunker2010@users.noreply.github.com>
2021-10-21 20:32:53 +0300

    "цилиндр" вынесен в ресурсы

--- app/src/main/java/com/example/thermalgapcalc_compose/presentation/utils/CalcUtils.kt
+++ app/src/main/java/com/example/thermalgapcalc_compose/presentation/utils/CalcUtils.kt
@@ -3,6 +3,7 @@ package com.example.thermalgapcalc_compose.presentation.utils
 import com.example.thermalgapcalc_compose.presentation.utils.CalcUtils.DeviationStatuses.BAD_STATE
 import com.example.thermalgapcalc_compose.presentation.utils.CalcUtils.DeviationStatuses.GOOD_STATE
 import com.example.thermalgapcalc_compose.presentation.utils.CalcUtils.DeviationStatuses.NORMAL_STATE
+import java.lang.Exception
 import kotlin.math.roundToInt
 
 object CalcUtils {
@@ -19,8 +20,14 @@ object CalcUtils {
     fun calcGapDeviation(normalGap: Float, gap: Float) =
         (((gap - normalGap) * 100.0F).roundToInt() / 100.0F)
 
+
     private fun calcDeviationPercent(gapTolerance: Float, gap: Float): Float {
-        return (((gap / gapTolerance * 100) * 100.0F).roundToInt() / 100.0F)
+        // TODO: 21.10.2021 Разобраться с ошибкой
+        return try {
+            (((gap / gapTolerance * 100.0f) * 100.0F).roundToInt() / 100.0F)
+        } catch (e: Exception) {
+            0.0f
+        }
     }
 
     fun getDeviationStatus(gapTolerance: Float, deviation: Float): String {
